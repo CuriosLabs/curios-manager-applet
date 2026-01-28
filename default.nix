12072b7f5947841a9ca210ebd8d073a24d8d4db3
VideoCurio <david@videocurio.com>
2026-01-28 17:22:33 +0100

    Hot fix

--- default.nix
+++ default.nix
@@ -3,7 +3,7 @@
 { lib, stdenv, fetchFromGitHub, rustPlatform, just, libcosmicAppHook }:
 rustPlatform.buildRustPackage rec {
   pname = "curios-manager-applet";
-  version = "0.1.O";
+  version = "0.1.0";
 
   src = fetchFromGitHub {
     owner = "CuriosLabs";

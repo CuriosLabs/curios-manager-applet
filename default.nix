82113aed2ad83fa4f3432ef33344b7e76d8b1ab8
VideoCurio <david@videocurio.com>
2026-05-18 11:21:30 +0200

    release: update hashes for 0.2.2

--- default.nix
+++ default.nix
@@ -10,10 +10,10 @@ rustPlatform.buildRustPackage rec {
     owner = "CuriosLabs";
     repo = "curios-manager-applet";
     tag = "${version}";
-    hash = "";
+    hash = "sha256-V3IIuYkC3gjDmqF2UWwHhnnk4L5ztE0YauCbQBTKTQc=";
   };
 
-  cargoHash = "sha256-UQSPocgLACVOmOPkFvsxfNqdXQkGDmbU6B96QSOQeHs=";
+  cargoHash = "sha256-NCkY6OBuz3u/WRbB1z+rllvcYhYVmVvlWtx8J9YKfTc=";
 
   nativeBuildInputs = [ pkg-config just libcosmicAppHook ];
 

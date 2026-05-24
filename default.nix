3db783c83946bb9d01048c86fc53aca0885441f3
VideoCurio <david@videocurio.com>
2026-05-24 10:40:44 +0200

    release: update hashes for 0.2.3

--- default.nix
+++ default.nix
@@ -10,7 +10,7 @@ rustPlatform.buildRustPackage rec {
     owner = "CuriosLabs";
     repo = "curios-manager-applet";
     tag = "${version}";
-    hash = "";
+    hash = "sha256-YVvYSKpKqWFDdhJhFqnuow8FTaHLMUpI6LGZUmuSeqk=";
   };
 
   cargoHash = "";

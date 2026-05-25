95b7766b767ca3f724869775262ed9db69e83356
VideoCurio <david@videocurio.com>
2026-05-25 13:05:06 +0200

    release: update hash for 0.2.4

--- default.nix
+++ default.nix
@@ -10,7 +10,7 @@ rustPlatform.buildRustPackage rec {
     owner = "CuriosLabs";
     repo = "curios-manager-applet";
     tag = "${version}";
-    hash = "";
+    hash = "sha256-BIeSZuMbYL3mFNK39JRDV4q0p+14ZTGCKN4K3fJCdyQ=";
   };
 
   cargoLock = {
